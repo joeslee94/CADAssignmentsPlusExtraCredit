a7c9b6138ab09a9b838adb18590955b7357ea6ed
Joe Lee <75293272+joeslee94@users.noreply.github.com>
2021-04-09 03:01:31 +0900

    Add files via upload

---
+++ Data Structures/BinarySearchTrees.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _28MAR21_Noelle_Notes
+{
+    //we need a node class
+    class Node
+    {
+        //property
+        public int value { get; set; }
+        public Node leftChild { get; set; }
+        public Node rightChild { get; set; }
+
+        //constructor
+        public Node(int value)
+        {
+            this.value = value;
+            this.rightChild = null;
+            this.leftChild = null;
+        }
+
+    }
+
+    //a tree has root and has children
+    //a BINARY tree has a root/node and has no more than 2 children
+    //BINARY SEARCH TREE -- sorting it in a way to make the search easier What we are using
+    //There are standard rules
+    //Left is less than
+    //Right is greater than equal to
+    //Left and right adds have conditions and then
+    //IF asked hey find x number in array, we have to touch each number O(n)
+    //IN binary search tree we only need to go through the conditions of the tree O(log n)
+
+    class BinarySearchTrees
+    {
+        public Node root { get; set; }
+
+        public BinarySearchTrees(int value)
+        {
+            root.value = value;
+            root.rightChild = null;
+            root.leftChild = null;
+        }
+
+        public void Add(int value)
+        {
+            //create node
+            Node newNode = new Node(value);
+
+            //if root is null this becomes the null
+            if (this.root == null)
+            {
+                this.root = newNode;
+            }
+            else
+            {
+                Node leftOrRightPointer = this.root;
+                bool addedSuccesful = false; //did not add node yet //makes it explicit
+
+                while (!addedSuccesful) //WHILE we have not added the node //CAN BE input as true
+                {
+                    if (newNode.value < leftOrRightPointer.value) // && leftOrRight.leftChild == null) //easier to break up
+                    {
+                        if (leftOrRightPointer.leftChild == null) //ask one question at a time! is the left null?
+                        {
+                            leftOrRightPointer.leftChild = newNode; //we have added the node
+                            addedSuccesful = true; //change to true //can either break (out of the loop) or return (out of the method)
+                        }
+                        else
+                        {
+                            leftOrRightPointer = leftOrRightPointer.leftChild;
+                        }
+                    }
+                    else //(leftOrRight.value <= newNode.value)// && leftOrRight.rightChild == null)
+                    {
+                        if (leftOrRightPointer.rightChild == null)
+                        {
+                            leftOrRightPointer.rightChild = newNode;
+                            addedSuccesful = true;
+                        }
+                        else
+                        {
+                            leftOrRightPointer = leftOrRightPointer.rightChild;
+                        }
+                    }
+                }
+            }
+        }
+
+        //FIND
+        public Node Find(int value)
+        {
+            //Node findValue = new Node(value);
+            //string message = "";
+            Node leftOrRightPointer = this.root;
+            if (this.root == null)
+            {
+                throw new Exception("There are no nodes with valid values for you to find.");
+            }
+            else
+            {
+                //int leftturn = 0;
+                //int rightturn = 0;
+                while(leftOrRightPointer.value != value)
+                {
+                    if (leftOrRightPointer.value == value)
+                    {
+                        //message += "The Node with the specified value is the root.";
+                        return leftOrRightPointer;
+                    }
+                    
+                    //left of root or node is less than
+                    if (value < leftOrRightPointer.value && leftOrRightPointer.leftChild != null)
+                    {
+                        leftOrRightPointer = leftOrRightPointer.leftChild;
+                        //leftturn++;
+                        if (leftOrRightPointer.value == value)
+                        {
+                            return leftOrRightPointer;
+                        }
+                        else
+                            continue;
+
+                    }
+
+                    //right is greater than or equal to
+                    if (value >= leftOrRightPointer.value && leftOrRightPointer.rightChild != null)
+                    {
+                        leftOrRightPointer = leftOrRightPointer.rightChild;
+                        //rightturn++;
+                        if (leftOrRightPointer.value == value)
+                        {
+                            return leftOrRightPointer;
+                        }
+                        else
+                            continue;
+                    }
+                }
+
+                //message += $"After going left {leftturn} times and right {rightturn} times, your Node with the specified value was found.";
+            }
+            return leftOrRightPointer;
+        }
+        public void Remove(int value)
+        {
+            //trickiest = remove node WHAT ABOUT THE KIDS THINK OF THEM AND HOW TO LINK THEM
+            //WHAT if we remove the root??
+            //Node compareValue = new Node(value);
+            
+            if (this.root == null)
+            {
+                throw new Exception("You do not have a node with a value to remove.");
+            }
+            else
+            {
+
+                if (this.root.value == value)
+                {
+                    Node originalRootHolder = this.root;
+                    Node nodeRight = originalRootHolder.rightChild; //OG root right
+                    //Node nodeLeft = originalRootHolder.leftChild; // OG root left
+                    Node pointer = nodeRight;
+                    //I want to find on the right side of the original tree where the pointer.left can be put on.
+                    pointer = pointer.leftChild;
+                    //holding onto the left child of the OG root's right child
+                    int lessThanPointer = pointer.value - 1;
+                    Node whereToAddPointer = Find(lessThanPointer);
+                    whereToAddPointer.leftChild = pointer;
+
+                    //made the left node of the OG's right node null then took the OG left null and equated it to the OG's right Node's left
+                    nodeRight.leftChild = null;
+                    nodeRight.leftChild = originalRootHolder.leftChild;
+
+                    //made the OG's right node the root
+                    this.root = nodeRight;
+                }
+
+                Node nodeToRemove = Find(value);
+                Node orphanRight = nodeToRemove.rightChild;
+                Node orphanLeft = nodeToRemove.leftChild;
+
+                Node newParentforOrphanRight = Find(orphanRight.value - 1); //but we need to find a node with a null
+                Node newParentforOrphanLeft = Find(orphanLeft.value + 1); //but we need to find a node with a null
+            }
+        }
+
+    }
+}
---
+++ Data Structures/LinkedLists.cs
@@ -0,0 +1,191 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _21MAR21_Noelle
+{
+    class Node
+    {
+        public int value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            this.value = value;
+            this.Next = null;
+        }
+
+        //we need a way to manage the link between nodes
+        //NODE's purpose is to hold the value and know its neighbors address
+        //Another class, a different concept, to manage the nodes
+    }
+
+    class LinkedList
+    {
+        public Node Head { get; set; }
+        public LinkedList()
+        {
+            this.Head = null;
+        }
+        public int GetValueAtIndex(int index) //takes the node at the index and then provides the value stored on the node
+        {
+            //Initialize an integer for setting the value at Index
+            int valueAtIndex = 0;
+            //have to find the index of the node
+            Node runner = this.Head;
+            //however, we cannot just index the nodes directly because we did not put an index value
+
+            //if index == 0 what happens?
+            if (index < 0)
+            {
+                throw new Exception("Enter a valid index");
+            }
+            else if (index == 0)
+            {
+                return runner.value;
+            }
+            else
+            {
+                //a for loop will give us that index given the int value it will iterate through/until
+                //iterator is i++
+                for (int i = 1; i <= index; i++)
+                {
+                    //STARTS at HEAD and this statement will be 
+                    runner = runner.Next;
+                    //what happens when the index is larger than the length of the list
+                    //what happens when the index is at the end of the list
+                    if (runner.Next == null)
+                    {
+                        valueAtIndex = runner.value;
+                    }
+                }
+            }
+
+            return valueAtIndex;
+        } 
+        public void AddValuetoNodeANDNodetoEnd(int value) //Adding value to the node and adding the node to the linkd list
+        {
+            //have to create a new node no matter what
+            Node newNode = new Node(value); //Nodes value is set here
+
+            //if the head is null, set the head, which is a node, to the value so that the head references the node with the inputted value
+                //WE DO NOT WANT TO CHANGE THE HEAD
+            if (this.Head == null)
+            {
+                this.Head = newNode;
+                return; //makes the head into the new node that is created
+            }
+
+            //CREATE A TEMP VARIABLE that is a pointer that iterates to the last node. this is evaluated when the runner says its next is null
+            Node runner = this.Head;
+            while (runner.Next != null)
+            {
+                runner = runner.Next;
+            }
+
+            //update the last node's next to point to the newly created node.
+            runner.Next = newNode;
+        } 
+        public void AddtoFront(int value)
+        {
+            //Must create a new node to add to the head
+            Node newHead = new Node(value);
+            //Make a new node
+            
+            //this.Head.Next = placeHolderNode; //O(head/placeholder) --> O(head)--O(placeHolder)
+            //this.Head = newHead; //O(newHead)--O(placeHolder)
+            
+            //Creating a node holding the Head of the linked list
+            Node placeHolderNode = this.Head; //do not need this
+            //point new node to the current Head
+            //update the head 
+            //O(Head) no value means O(newHead) becomes O(Head)
+            if (this.Head == null)
+            {
+                this.Head = newHead; //making th eHead the node that was created
+            }
+            else
+            {
+                this.Head.Next = placeHolderNode; //O(head/placeholder) --> O(head)--O(placeHolder)
+                this.Head = newHead; //O(newHead)--O(placeHolder)
+            }
+        }
+        public int RemoveAt(int index)
+        {
+            //have to find the index of the node
+            Node runner = this.Head;
+            //however, we cannot just index the nodes directly because we did not put an index value
+            
+            //if index == 0 what happens?
+            if (index == 0)
+            {
+                this.Head = runner.Next;
+                return runner.value;
+            }
+
+            //a for loop will give us that index given the int value it will iterate through/until
+            //within the for loop we need the runner to equal the runner.Next?
+            //length is set to index
+            //iterator is i++
+            for (int i = 0; i < index - 1; i++)
+            {
+                runner = runner.Next;
+                //what happens when the index is larger than the length of the list
+                //what happens when the index is at the end of the list
+                if (runner.Next == null)
+                {
+                    throw new Exception("The is not a valid index.");
+                }
+            }
+
+            //THE NEXT runner.NEXT = is the item to remove
+            //runner is at the node before the node we want to skip
+            //nodeToRemove is going to be runner.Next
+            Node nodeToChange = runner.Next;
+            //nodeToRemove.Next is the node I want the runner to link with
+            //runner.Next = nodetoremove.Next?
+            runner.Next = nodeToChange.Next;
+            return nodeToChange.value;
+        }
+        public int UpdateAt(int index, int value)
+        {
+            //Initialize an integer for setting the value at Index
+            int valueAtIndexBeforeUpdate = 0;
+            //have to find the index of the node
+            Node runner = this.Head;
+            //however, we cannot just index the nodes directly because we did not put an index value
+
+            //if index == 0 what happens?
+            if (index < 0)
+            {
+                throw new Exception("Enter a valid index");
+            }
+            else if (index == 0)
+            {
+                valueAtIndexBeforeUpdate = runner.value;
+                runner.value = value;
+                return valueAtIndexBeforeUpdate;
+            }
+            else
+            {
+                //a for loop will give us that index given the int value it will iterate through/until
+                //iterator is i++
+                for (int i = 1; i <= index; i++)
+                {
+                    runner = runner.Next;
+                    if (i == index - 1)
+                    {
+                        valueAtIndexBeforeUpdate = runner.value;
+                        runner.value = value;
+                    }
+                    else
+                        continue;
+                }
+            }
+
+            return valueAtIndexBeforeUpdate;
+        }
+
+    }
+}
