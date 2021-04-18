539b0ceb9e449b3e0e55640fb321cd7da7aa70b9
Joe Lee <75293272+joeslee94@users.noreply.github.com>
2021-04-18 20:16:21 +0900

    Add files via upload

---
+++ Data Structures/ArrayStackStructure.cs
@@ -0,0 +1,83 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks
+{
+    class ArrayStackStructure<T> : IEnumerable<T>
+    {
+        private T[] _genArray;
+        public int Count { get; private set; }
+        public bool IsEmpty => Count == 0;
+
+        public ArrayStackStructure()
+        {
+            const int defaultSize = 10;
+            _genArray = new T[defaultSize];
+        }
+
+        public ArrayStackStructure(int capacity)
+        {
+            _genArray = new T[capacity];
+        }
+
+        public void Push(T genVal)
+        {
+            if (_genArray.Length == Count)
+            {
+                T[] doubleGenArray = new T[Count * 2];
+                Array.Copy(_genArray, doubleGenArray, Count);
+                _genArray = doubleGenArray;
+            }
+
+            //_genArray[Count] = genVal;
+            //Count++;
+            
+            _genArray[Count++] = genVal;
+        }
+
+        public T Pop()
+        {
+            if(IsEmpty)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            //Make a generic item hold the top item within the stack
+            T poppedItem = _genArray[Count - 1];
+
+            ////make the position at Count in stack the default value
+            //_genArray[Count] = default(T);
+            ////decrement Count
+            //Count--;
+            _genArray[Count--] = default(T);
+
+            return poppedItem;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            return _genArray[Count - 1];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return _genArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Data Structures/LinkedListStackStructure.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks
+{
+    class LinkedListStackStructure<T> : IEnumerable<T>
+    {
+        public int Count { get; private set; }
+        public bool IsEmpty => Count == 0;
+        public Node<T> Next { get; set; }
+        public Node<T> Head { get; set; }
+        public Node<T> Tail { get; set; }
+
+        public void Push(T genVal)
+        {
+            if (IsEmpty)
+            {
+                Node<T> firstNodeToAdd = new Node<T>(genVal);
+                Head = Tail = firstNodeToAdd;
+            }
+            else
+            {
+                Node<T> nodeToAdd = new Node<T>(genVal);
+                Tail.next = nodeToAdd;
+                Tail = nodeToAdd;
+            }
+
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            Node<T> nodeToPop;
+
+            if (Count == 1)
+            {
+                nodeToPop = Head;
+                Head = Tail = null;
+            }
+            else
+            {
+                nodeToPop = Tail;
+
+                Node<T> nodePointer = Head;
+
+                for (int i = 0; i < Count-2; i++)
+                {
+                    nodePointer = nodePointer.next;
+                }
+
+                Tail = nodePointer;
+                nodePointer.next = null;
+                
+                Count--;
+            }
+            
+            return nodeToPop.val;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            return Tail.val;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> currentNode = Head;
+            while(currentNode != null)
+            {
+                yield return currentNode.val;
+                currentNode = currentNode.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
